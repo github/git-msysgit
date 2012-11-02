martinlenting44/git

Let measure_time() output also <file>:<lineno>
The lazy way to measure time is now

	measure_time("");

since that will output

	<file>:<lineno>: : <time>

for every such call (providing enough information to pinpoint performance
bottlenecks).

Since it requires the __VA_ARGS__ feature which this developer suspects to
be unsupported by Visual C++ versions currently in use, this functionality
is guarded by an '#ifdef __GNUCC__'.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

compat/mingw.h
@@ -461,4 +461,9 @@ extern int err_win_to_posix(DWORD winerr);
 extern const char *get_windows_home_directory();
 #define get_home_directory() get_windows_home_directory()
 
-extern int measure_time(const char *fmt, ...);
+extern int measure_time_aux(const char *file, int lineno, const char *fmt, ...);
+#ifdef __GNUCC__
+#define measure_time(...) \
+	measure_time_aux(__FILE__, __LINE__, __VA_ARGS__)
+#endif
+

github/git-msysgit

compat/mingw.h
@@ -459,4 +459,8 @@ extern int err_win_to_posix(DWORD winerr);
 extern const char *get_windows_home_directory();
 #define get_home_directory() get_windows_home_directory()
 
-extern int measure_time(const char *fmt, ...);
+extern int measure_time_aux(const char *file, int lineno, const char *fmt, ...);
+#ifdef __GNUC__
+#define measure_time(...) \
+	measure_time_aux(__FILE__, __LINE__, __VA_ARGS__)
+#endif